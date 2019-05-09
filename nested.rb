d3a2faad532a2468d3e8124ad2ee2d13fedf2215
Ititi Obidah <iobidah9@gmail.com>
2019-05-09 13:18:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages] = "Assembly".push
+    programmer_hash[:dennis_ritchie][:languages] = ("Assembly").push
 end
